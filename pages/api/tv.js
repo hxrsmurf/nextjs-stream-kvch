6b84780fa6574d68ac5653cc863b92dbb5710a5e
Kevin Murphy <kevin@hxrsmurf.info>
2022-05-15 19:33:29 -0400

    basic TV poster query

--- pages/api/tv.js
+++ pages/api/tv.js
@@ -1,8 +1,33 @@
 import tmdb from "./components/tmdb"
 
-export default function tv(req,res) {
-    const result = tmdb()
+const tv_ids = [37419]
+
+export default async function tv(req,res) {
+    const api = process.env.TMDB_API_KEY
+
+    const base_url = 'https://api.themoviedb.org/3'
+    const image_url = 'https://image.tmdb.org/t/p/w500'
+    const headers = {
+        Authorization: 'Bearer ' + api,
+        'Content-Type': 'application/json;charset=utf-8'
+    }
+
+    const response_tv  = await fetch(base_url + '/tv/' + tv_ids, {headers})
+    const result_tv = await response_tv.json()
+    const result_tv_poster = image_url + result_tv.poster_path
+
+    const episodes = []
+    for (const s of result_tv.seasons){
+        const url = base_url + '/tv/' + tv_ids + '/season/' + s.season_number
+        const response_season = await fetch(url, {headers})
+        const result_season = await response_season.json()
+        episodes.push(result_season)
+    }
+
     res.status(200).json(
-        result
+        {
+            result_tv_poster,
+            episodes
+        }
     )
 }
--- pages/tv.js
+++ pages/tv.js
@@ -1,13 +1,39 @@
 import { Container } from "react-bootstrap";
 import Navigation from "../components/Navigation";
 
-export default function tv() {
+import { loadtv } from "../lib/fetch-tv";
+
+export default function tv(props) {
+
+    const tv_poster = props.tv['result_tv_poster']
+    const episodes = props.tv['episodes']
+
   return (
     <>
         <Navigation/>
-        <Container>
-            hello
+        <Container className='mt-5'>
+            <img src={tv_poster}></img>
+            {episodes.map((episode, index) =>(
+              <div key={index}>
+
+                {episode.episodes.map((e, index)=> (
+                  <div key={index}>
+                    {e.name}
+
+                  </div>
+                ))}
+                </div>
+            ))}
         </Container>
     </>
   )
+}
+
+export async function getStaticProps(){
+  const tv = await loadtv()
+  return {
+    props: {
+      tv
+    }
+  }
 }
