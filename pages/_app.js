e223130dbf19d66deeea4f1f1c4c864c2b4abcfa
Kevin Murphy <kevin@hxrsmurf.info>
2022-05-15 23:21:32 -0400

    Add navigation to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import Navigation from '../components/Navigation'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navigation/>
+      <Component {...pageProps}/>
+    </>
+  )
 }
 
 export default MyApp
--- pages/tv.js
+++ pages/tv.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col, Image } from "react-bootstrap";
-import Navigation from "../components/Navigation";
 
 import { loadtv } from "../lib/fetch-tv";
 
@@ -7,17 +6,15 @@ const image_url = 'https://image.tmdb.org/t/p/w500'
 
 export default function tv( {tv} ) {
   //console.log(tv)
-
   return (
     <>
-        <Navigation/>
         <Container className='mt-5'>
         <Container>
 
         <Row>
             {tv.results.map((result, index)=> (
               <Col xs={4} md={6} key={index}>
-                <a href={'zoids/' + result.id}>
+                <a href={'tv/' + result.id}>
                     <img src={image_url + result.poster_path} className='img-fluid rounded mt-2 border border-dark' />
                 </a>
               </Col>
