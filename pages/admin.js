516b046a90518fc9a30c98bc2302e6125a79fcf3
Kevin Murphy <kevin@hxrsmurf.info>
2022-05-22 11:39:24 -0400

    Add firebase firestore admin table

--- pages/admin.js
+++ pages/admin.js
@@ -1,18 +1,39 @@
-import { AuthAction, useAuthUser, withAuthUser } from "next-firebase-auth"
-import { Container } from "react-bootstrap"
+import { AuthAction, useAuthUser, withAuthUser, getFirebaseAdmin } from "next-firebase-auth"
+import { Container, Table } from "react-bootstrap"
 import Loader from "../components/Loader"
 import checkAuth from "../utils/checkAuth"
 import initAuth from "../utils/initAuth"
 
 initAuth()
 
-export function admin() {
+export function admin({users}) {
   const AuthUser = useAuthUser()
   checkAuth(AuthUser)
   return (
     <>
         <Container className='mt-5'>
             Admin dashboard for {AuthUser.email}
+            <p>
+            <h1>Users</h1>
+            <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                  <th>
+                    Username
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                    <tr>
+                      <td>
+                      {user.user}
+                      </td>
+                    </tr>
+                ))}
+              </tbody>
+            </Table>
+            </p>
         </Container>
     </>
   )
@@ -24,3 +45,15 @@ export default withAuthUser({
     authPageURL: '/',
     LoaderComponent: Loader,
   })(admin)
+
+export async function getServerSideProps() {
+  const db = getFirebaseAdmin().firestore()
+  const doc = await db.collection('users').get()
+  return {
+    props: {
+      users: doc.docs.map((d)=> {
+        return {...d.data(), key: d.id}
+      }),
+    },
+  }
+}
--- utils/initAuth.js
+++ utils/initAuth.js
@@ -15,9 +15,12 @@ const initAuth = () => {
     firebaseAdminInitConfig: {
       credential: {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        clientEmail: process.env.FIREBASE_IAM_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+        : undefined
       },
+      databaseURL: 'https://stream-kvch.firebaseio.com',
     },
     firebaseClientInitConfig: {
       apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY, // required
